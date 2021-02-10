84cb952cd99dfe6510dbc3c976b4257fedfc6259
Ashley U <ashley.umetsu@gmail.com>
2021-02-09 21:32:17 -0600

    started creation of some methods and added comments

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,4 +6,16 @@
  	constructor() {
  		this.phrase = phrase.toLowerCase();
  	}
+ 	// Adds letter placeholders to the display when the game starts
+ 	addPhraseToDisplay() {
+
+ 	}
+ 	// Checks to see if the letter selected by the player matches a letter in the phrase.
+ 	checkLetter() {
+
+ 	}
+ 	// Reveals the letter(s) on the board that matches the player's selection
+ 	showMatchedLetter() {
+
+ 	}
  }
