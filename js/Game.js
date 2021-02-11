fd647e2bf46d5a5bb0c0825c318c05e60226b3f6
Ashley U <ashley.umetsu@gmail.com>
2021-02-10 20:45:42 -0600

    Bug fix

--- js/Game.js
+++ js/Game.js
@@ -5,7 +5,7 @@
  class Game {
  	constructor () {
  	this.missed = 0;
- 	this.phrases = ['Shenanigans', 'Bamboozle', 'Finagle', 'Cattywampus', 'Skedaddle'];
+ 	this.phrases = [new Phrase("Shenanigans"), new Phrase("Cattywampus"), new Phrase("Bamboozle"), new Phrase("Skedaddle"), new Phrase("Gnarly")];
  	this.activePhrase = null;
  	}
 }
